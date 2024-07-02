9a159590298c995873ae863ef94f01fbe9f8b928
Energycake <energycake85@gmail.com>
2024-07-02 22:35:57 +0300

    hand.py

--- hand.py
+++ hand.py
@@ -3,22 +3,17 @@ from scr.deck import Deck
 class Hand:
     def __init__(self):
         self.cards_hand = []
-        self.cards_hand_text = []
 
     def current_cards(self):
-        for i in range (len(self.cards_hand_text)):
+        for i in range (len(self.cards_hand)):
             print(f'{i+1}.{self.cards_hand[i]}')
     def add_card(self,card):
-        self.cards_hand.append(card)
-        self.cards_hand_text.append(str(card))
-    def select_card(self, point):
-        point =
+        self.cards_hand.append(str(card))
 
-    def remove_card(self, point):
-        self.cards_hand_text.pop(point)
-        return self.cards_hand.pop(point)
+    def remove_card(self, card):
+        return self.cards_hand.pop(card - 1)
 
-    def last_card(self):
+    def one_card(self):
         if len(self.cards_hand) == 1:
             print("uno!")
     def no_cards(self):
