147308341965a7d6010f1a8a8e1b1f252c57d81a
Energycake <energycake85@gmail.com>
2024-07-02 22:40:14 +0300

    test_hand.py

--- test_hand.py
+++ test_hand.py
@@ -1,13 +1,16 @@
 from scr.card import Card
 from scr.hand import Hand
+
 def test_init():
     lst = Hand()
     assert lst.cards == []
+
 def test_repr():
     lst = Hand()
     lst.cards.append(Card('r', 6))
     lst.cards.append(Card('g', 4))
     assert repr(lst) == 'r6 g4'
+
 def test_add():
     lst = Hand()
     c = Card('r', 6)
@@ -15,7 +18,7 @@ def test_add():
     c = Card('g', 4)
     lst.add(c)
     c = Card('y', 2)
-    lst.add(c)
+    lst.add(c) 
 
     assert lst.cards[0] == Card('r', 6)
     assert lst.cards[1] == Card('g', 4)
