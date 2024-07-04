58fdcc49bc98c9ba1a8622cd87928f0c8a190d8a
Energycake <energycake85@gmail.com>
2024-07-04 20:51:49 +0300

    hand and hand_test

--- hand.py
+++ hand.py
@@ -1,24 +1,22 @@
-from scr.card import Card
-from scr.deck import Deck
+
 class Hand:
     def __init__(self):
         self.cards_hand = []
-        self.cards_hand_text = []
+
 
     def current_cards(self):
-        for i in range (len(self.cards_hand_text)):
+        for i in range (len(self.cards_hand)):
             print(f'{i+1}.{self.cards_hand[i]}')
     def add_card(self,card):
         self.cards_hand.append(card)
-        self.cards_hand_text.append(str(card))
+        self.cards_hand.append(str(card))
 
     def __eq__(self, other):
-        return self.cards == other.cards
+        return self.cards_hand == other.cards
 
     
     def remove_card(self, point):
-        self.cards_hand_text.pop(point)
-        return self.cards_hand.pop(point)
+        return self.cards_hand.remove(point)
 
     def last_card(self):
         if len(self.cards_hand) == 1:
--- test_hand.py
+++ test_hand.py
@@ -1,25 +1,26 @@
-from scr.card import Card
-from scr.hand import Hand
+from card import Card
+from hand import Hand
+
 
 def test_init():
     lst = Hand()
-    assert lst.cards == []
+    assert lst.cards_hand == []
 
 def test_repr():
     lst = Hand()
-    lst.cards.append(Card('r', 6))
-    lst.cards.append(Card('g', 4))
+    lst.cards_hand.append(Card('r', 6))
+    lst.cards_hand.append(Card('g', 4))
     assert repr(lst) == 'r6 g4'
 
 def test_add():
     lst = Hand()
     c1 = Card('r', 6)
-    lst.add(c1)
+    lst.add_card(c1)
     c2 = Card('g', 4)
-    lst.add(c2)
+    lst.add_card(c2)
     c3 = Card('y', 2)
-    lst.add(c3) 
+    lst.add_card(c3)
 
-    assert lst.cards[0] == Card('r', 6)
-    assert lst.cards[1] == Card('g', 4)
-    assert lst.cards[2] == Card('y', 2)
+    assert lst.cards_hand[0] == Card('r', 6)
+    assert lst.cards_hand[1] == Card('g', 4)
+    assert lst.cards_hand[2] == Card('y', 2)
