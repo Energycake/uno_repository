e433b36118635dbb29113fc7fe0eb572fa129c5b
Energycake <energycake85@gmail.com>
2024-07-04 21:52:50 +0300

    hand

--- test_hand.py
+++ test_hand.py
@@ -24,6 +24,19 @@ def test_add():
     assert lst.cards_hand[0] == Card('r', 6)
     assert lst.cards_hand[1] == Card('g', 4)
     assert lst.cards_hand[2] == Card('y', 2)
+    
+def test_load_card():
+    card_list = [Card('blue', 2), Card('yellow', 4), Card('red', 7)]
+    hand = Hand.load_card('b2 y4 r7')
+    assert hand.cards_hand == card_list
+    
+def test_eq():
+    hand1 = Hand.load_card('b2 y4 r7')
+    hand2 = Hand.load_card('b2 y4 r7')
+    hand3 = Hand.load_card('y7 r1 g5')
+    assert hand1 == hand2
+    assert hand1 != hand3
+
 
 def test_remove():
     lst = Hand()
