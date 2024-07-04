55ebd328bc3933a6b0a0771fca82674714ecf96f
Energycake <energycake85@gmail.com>
2024-07-04 20:25:47 +0300

    card

--- card.py
+++ card.py
@@ -1,17 +1,20 @@
+class CardException(Exception):
+    pass
+
 class Card:
     colors=('red','green','blue','yellow')
     numbers=list(range(10))+ list (range(1, 10))
     dcolors={color[0]: color for color in colors}
 
     def __init__(self, color: str, number: int):
-         if color not in Card.dcolors.keys() and color not in Card.dcolors.values() or  number not in Card.numbers:
-                raise CardException()
-            self.color=color
-            self.number=number
+        if color not in Card.dcolors.keys() and color not in Card.dcolors.values() or  number not in Card.numbers:
+            raise CardException()
+        self.color=color
+        self.number=number
 
     def __eq__(self, other):
         if  self.color == other.color or self.number == self.number:
-        return True
+            return True
 
     def __repr__(self):
         letter=self.color[0]
@@ -34,14 +37,13 @@ class Card:
             return self.color == other.color or self.number == other.number
 
     @staticmethod
-
-    def all_cards():
+    def all_cards(colors = None, numbers = None):
         """Возвращает все карты
         :param colors:
         :param numbers:"""
-         if colors is None:
+        if colors is None:
             colors = Card.colors
-         if numbers is None:
+        if numbers is None:
             numbers = Card.numbers
 
 
